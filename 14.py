23b647608d9a6f8162e99ece6b70bc1aa8cd3f39
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-11-21 20:18:17 +0900

    시계

--- 14.py
+++ 14.py
@@ -1,1 +1,15 @@
-a, b , c= map(int, input().split())
+a, b = map(int, input().split())
+c = input()
+c = int(c)
+m = b+c
+if (m >= 60) :
+    a += m/60
+    if a >= 24 : a = 0
+    
+    m = m%60
+    print("%d "%a, end="")
+    print("%d"%m)
+
+else :
+    print("%d "%a, end="")
+    print("%d"%m)
