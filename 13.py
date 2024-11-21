a400fbf9d1aa933323e75237f70d838c1bf6d37e
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2024-11-21 20:08:21 +0900

    망했다

---
+++ 13.py
@@ -0,0 +1,16 @@
+import sys
+s = input()
+s = int(s)
+i = 0
+while i<s :
+    i+=1
+    a, b = map(int, input().split())
+    su = a+b
+    print("Case #",end="")
+    print(i,end="")
+    print(":%d"%a,end="")
+    print("+%d ="%b,end = "")
+    print("%d"%su)
+    
+
+sys.exit(1)
